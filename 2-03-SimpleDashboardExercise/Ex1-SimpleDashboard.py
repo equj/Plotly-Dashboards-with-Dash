74f6b233bfbf81f8845255aa1e016f1d8e857042
equj <30753904+equj@users.noreply.github.com>
2018-11-08 11:14:11 -0500

    new classes done

--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -8,38 +8,38 @@
 ######
 
 # Perform imports here:
-
-
-
-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
 
 
 # Launch the application:
-
+app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
-
+df = pd.read_csv('../data/OldFaithful.csv')
 
 # Create a Dash layout that contains a Graph component:
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.layout = html.Div([dcc.Graph(id = 'scatterplot',
+                figure = {'data':[
+                        go.Scatter(
+                        x = df['X'],
+                        y = df['Y'],
+                        mode = 'markers',
+                        marker = {
+                            'size':15,
+                            'color': 'rgb(0, 100, 250)',
+                            'symbol':'circle',
+                            'line':{'width':3}
+                        }
+                        )],
+                'layout': go.Layout(title='Old Faithful Eruptions')}
+                )])
 
 # Add the server clause:
+if __name__ == '__main__':
+    app.run_server()
