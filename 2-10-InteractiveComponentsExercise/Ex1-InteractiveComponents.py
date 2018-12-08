2f676f619ec4d8e4f9c96ef1975bd74947fb818d
equj <30753904+equj@users.noreply.github.com>
2018-12-08 16:16:37 -0500

    done 2-10 ex

--- 2-10-InteractiveComponentsExercise/Ex1-InteractiveComponents.py
+++ 2-10-InteractiveComponentsExercise/Ex1-InteractiveComponents.py
@@ -5,31 +5,59 @@
 
 # Perform imports here:
 
-
-
+#               Step 1: import things
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 
 # Launch the application:
-
+#              Step 2: launch mani app
+app = dash.Dash()
 
 # Create a Dash layout that contains input components
 # and at least one output. Assign IDs to each component:
 
+#              Step 3: Creat Dash layout
+
+#build slider markers
+smarks = dict()
+
+for i in range (-5,7):
+    smarks[i] = str(i)
 
 
 
+app.layout = html.Div([
+    dcc.RangeSlider(
+        id = 'my-slider',
+        min=-5,
+        max=6,
+        marks=smarks,
+        value=[-5, 4]),
 
-
-
-
-
+    html.Br(),
+    html.Br(),
+    html.H1(id='my-text')
+], style={'fontFamily':'helvetica', 'fontSize':18})
 
 
 # Create a Dash callback:
+#           Step 4: Inact call back function
+@app.callback(
+        Output(component_id='my-text', component_property='children'),
+        [Input('my-slider', 'value')])
 
 
-
-
+#          Step 5: Define callback function
+def slider_callback(slider_val):
+        product = slider_val[0]*slider_val[1]
+        return 'slider product: {}'.format(product)
 
 
 # Add the server clause:
+#           Step 6: Run app server!!!
+
+if __name__ == '__main__':
+    app.run_server()
